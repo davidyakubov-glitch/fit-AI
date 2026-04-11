0095981b27b6543506f484b188015ce9f3b3dc53
ТвойНикНаГитхабе <_email>
2026-04-11 15:39:01 +0500

    Final free version without subscription

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,4 +1,3 @@
-// еуые
 import React, { useState, useCallback, useRef } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
